9c59ee9bd04208891ea7271e569a78402cc36a20
Chris San <christianes322@gmail.com>
2023-10-30 22:51:28 -0500

    created the seeds sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department(department_name)
+VALUES("Enineering"),("Sales"),("Finance"),("Legal"),("Marketing");
+
+INSERT INTO role(title, salary, department_id)
+VALUES("Engineer",100000, 1),("Manager",150000, 2),("Salesman",50000, 3),("CEO",200000, 4),
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Christian', 'Sanchez'1,2),('Joe', 'Mama'1,null),
