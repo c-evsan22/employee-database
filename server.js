4e448c6aca6102cf67a66e61679418ee8acc823c
Chris San <christianes322@gmail.com>
2023-10-31 22:29:25 -0500

    added the initial node links and js links in server js

---
+++ server.js
@@ -0,0 +1,6 @@
+const connection = require ( `./config/connection`);
+const inquirer = require (`inquirer`);
+const inputConnection = require ( `./java/valid`);
+const cTable = require (`console.table`);
+const chalk = require (`chalk`);
+const asciiArt = require (`figlet`);
