5f6a98288acccac056f413551a4cd77957f64f84
Chris San <christianes322@gmail.com>
2023-10-31 22:38:29 -0500

    created database connection and start of inital test

--- server.js
+++ server.js
@@ -4,3 +4,17 @@ const inputConnection = require ( `./java/valid`);
 const cTable = require (`console.table`);
 const chalk = require (`chalk`);
 const asciiArt = require (`figlet`);
+
+
+connection.connect((error)=>{
+    if(error) throw error;
+    console.log(chalk.blue.bold(`===============`));
+    console.log(``);
+    console.log(chalk.redBright.bold(figlet.textSync(`Employee Tracker`)));
+    console.log(``);
+    console.log(`     ` + chalk.redBright.bold(`Creator; Christian S.`));
+    console.log(``);
+    console.log(chalk.blue.bold(`===============`));
+    promptUser();
+});
+
